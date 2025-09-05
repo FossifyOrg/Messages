4b339881d637994e64d7487f16b483905f8b0da3
Naveen Singh <36371707+naveensingh@users.noreply.github.com>
2025-09-05 11:41:09 +0530

    fix: remove quick fix for clearing system drafts (#512)
    It was added for https://github.com/FossifyOrg/Messages/issues/274, but it's not needed anymore.

--- app/src/main/kotlin/org/fossify/messages/activities/MainActivity.kt
+++ app/src/main/kotlin/org/fossify/messages/activities/MainActivity.kt
@@ -59,7 +59,6 @@ import org.fossify.messages.databinding.ActivityMainBinding
 import org.fossify.messages.extensions.checkAndDeleteOldRecycleBinMessages
 import org.fossify.messages.extensions.clearAllMessagesIfNeeded
 import org.fossify.messages.extensions.clearExpiredScheduledMessages
-import org.fossify.messages.extensions.clearSystemDrafts
 import org.fossify.messages.extensions.config
 import org.fossify.messages.extensions.conversationsDB
 import org.fossify.messages.extensions.getConversations
@@ -287,7 +286,6 @@ class MainActivity : SimpleActivity() {
         checkWhatsNewDialog()
         storeStateVariables()
         getCachedConversations()
-        clearSystemDrafts()
         binding.noConversationsPlaceholder2.setOnClickListener {
             launchNewConversation()
         }
