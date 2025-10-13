6cbc94c2ef3bfb627bfcc23a31919c2a0556335f
Naveen Singh <snaveen935@gmail.com>
2025-10-13 22:58:19 +0530

    style: use constant for cache size

--- app/src/main/kotlin/org/fossify/messages/helpers/MessagingCache.kt
+++ app/src/main/kotlin/org/fossify/messages/helpers/MessagingCache.kt
@@ -3,6 +3,8 @@ package org.fossify.messages.helpers
 import android.util.LruCache
 import org.fossify.messages.models.NamePhoto
 
+private const val CACHE_SIZE = 512
+
 object MessagingCache {
-    val namePhoto = LruCache<String, NamePhoto>(512)
+    val namePhoto = LruCache<String, NamePhoto>(CACHE_SIZE)
 }
