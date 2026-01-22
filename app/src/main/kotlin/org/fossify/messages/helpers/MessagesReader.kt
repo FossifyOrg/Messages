63bab12d562eca2d55f68fcb96bee72faef3f175
Leonard Janis Robert König <ljrk@ljrk.org>
2026-01-23 00:40:09 +0100

    fix: SMS "locked" wrong value in export

--- app/src/main/kotlin/org/fossify/messages/helpers/MessagesReader.kt
+++ app/src/main/kotlin/org/fossify/messages/helpers/MessagesReader.kt
@@ -70,7 +70,7 @@ class MessagesReader(private val context: Context) {
                 val body = cursor.getStringValueOrNull(Sms.BODY)
                 val date = cursor.getLongValue(Sms.DATE)
                 val dateSent = cursor.getLongValue(Sms.DATE_SENT)
-                val locked = cursor.getIntValue(Sms.DATE_SENT)
+                val locked = cursor.getIntValue(Sms.LOCKED)
                 val protocol = cursor.getStringValueOrNull(Sms.PROTOCOL)
                 val read = cursor.getIntValue(Sms.READ)
                 val status = cursor.getIntValue(Sms.STATUS)
