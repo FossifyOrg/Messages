fe360f96e060ea83d4e61b908c705837c07c27e2
Adarsh Kannada <adarshkannada@outlook.com>
2026-05-24 23:47:52 +0530

    some changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ fun hasSigningVars(): Boolean {
 
 base {
     val versionCode = project.property("VERSION_CODE").toString().toInt()
-    archivesName = "messages-$versionCode"
+    archivesName = "sms_keeper-$versionCode"
 }
 
 android {
@@ -37,6 +37,7 @@ android {
         targetSdk = project.libs.versions.app.build.targetSDK.get().toInt()
         versionName = project.property("VERSION_NAME").toString()
         versionCode = project.property("VERSION_CODE").toString().toInt()
+        resValue("string", "app_launcher_name", "sms-keeper")
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
         }
@@ -65,6 +66,7 @@ android {
     buildFeatures {
         viewBinding = true
         buildConfig = true
+        resValues = true
     }
 
     buildTypes {
